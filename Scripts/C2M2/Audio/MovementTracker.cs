0019031a19ea051ba2463dc2c11e124a2f3ed496
Jaden Howard <jaseanhow@gmail.com>
2024-05-08 00:47:06 -0400

    test

---
+++ Scripts/C2M2/Audio/MovementTracker.cs
@@ -0,0 +1,43 @@
+﻿using C2M2.Utils.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+What I wanted to do with this script:
+    Attach this script to the Center Eye Anchor gameobject itself in the Editor Heirarchy as a component using "Add Component".
+    That would in theory, allow me to reference the 3d VR Player's position, which is equal to, "CenterEyeAnchor.transform.position".
+    I would then be able to save that position to a Vector3 variable called "position" in this class.
+    
+    However, when instantiating this class as an object in VertexAudio, and referencing "position" as it's saved in the Start() Method, there's no console output.
+    I print out the "position" variable in the Update() function of Vertex Audio, move around in VR, and there's no Debug.Log() console output.
+    
+    I am not sure why this is the case.
+    It may be: Human Error or a VR/Unity error.
+    
+ */
+
+
+public class MovementTracker : MonoBehaviour
+{
+
+    Vector3 position;
+    void Start()
+    {
+
+        position = this.transform.position;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public Vector3 GetPosition3d()
+    {
+        return position;
+    }
+
+
+}
---
+++ Scripts/C2M2/NeuronalDynamics/Interaction/NDGraph.cs
@@ -0,0 +1,56 @@
+﻿using C2M2.Interaction;
+using C2M2.Interaction.VR;
+using C2M2.NeuronalDynamics.Interaction;
+using C2M2.NeuronalDynamics.Interaction.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(GrabRescaler))]
+[RequireComponent(typeof(NDLineGraph))]
+public class NDGraph : NDInteractables
+{
+    public NDGraphManager GraphManager { get { return simulation.graphManager; } }
+
+    private GrabRescaler grabRescaler;
+    public NDLineGraph ndlinegraph;
+
+    //Jaden: Each Plot instance is equipped w/ an AudioSource Component.
+    public new AudioSource audio;
+
+    void Awake()
+    {
+        grabRescaler = GetComponent<GrabRescaler>();
+        ndlinegraph = GetComponent<NDLineGraph>();
+        var aud = this.gameObject.AddComponent<VertexAudio>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (simulation == null || GraphManager == null)
+        {
+            ndlinegraph.DestroyPlot();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GraphManager.graphs.Remove(this);
+    }
+
+    public override void Place(int index)
+    {
+        if (FocusVert == -1)
+        {
+            Debug.LogError("Invalid vertex given to NDLineGraph");
+            Destroy(this);
+        }
+        name = "Graph(" + simulation.name + ")[vert" + FocusVert + "]";
+        GraphManager.graphs.Add(this);
+    }
+
+    protected override void AddHitEventListeners()
+    {
+        HitEvent.OnHover.AddListener((hit) => grabRescaler.Rescale());
+    }
+}
